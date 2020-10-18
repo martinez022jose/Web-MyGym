d531818541c5e3ddb954b5b79c916e8bbb21d561
Jose_Martinez <martinez022jose@gmail.com>
2020-10-18 20:58:30 -0300

    Se modifica el nav(responsive design)

--- parts/nav.php
+++ parts/nav.php
@@ -1,13 +1,15 @@
 <!--Comienzo nav-->
 <header class="container-fluid fixed-top border-dark p-4 border-bottom bg-light">
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <i class='fas fa-volleyball-ball' style='font-size:36px'></i>
+    <div class="row">
+      <div class="col-lg-12">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <i class='fas fa-volleyball-ball tamanioFull'></i>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav m-auto tamanioHeader">
+          <ul class="navbar-nav m-auto tamanioHeader text-center centrar">
             <li class="nav-item">
               <a class="nav p-3 pr-5 orangeHover" href="#">Inicio</a>
             </li>
@@ -26,5 +28,8 @@
           </ul>
         </div>
       </nav>
+      </div>
+
+    </div>
 </header>
 <!--Fin nav-->
