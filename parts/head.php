af22b8ed041ca8153e66622fb243716b3e653b27
Jose_Martinez <martinez022jose@gmail.com>
2021-04-03 23:40:29 -0300

    cdn de fontawesome

--- parts/head.php
+++ parts/head.php
@@ -1,4 +1,6 @@
-
+<!--Fontawesome-->
+<script src="https://kit.fontawesome.com/59dceabbec.js" crossorigin="anonymous"></script>
+<!--Fin Fontawesome-->
 
 <!--Bootstrap4-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
